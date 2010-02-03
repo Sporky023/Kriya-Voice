308b08c0b1bf4e35bc9d0ed144b53a93ffea7474
Luke Griffiths <lookmeup>
2010-02-03 08:22:35 -0700

    initial commit

---
+++ test.rb
@@ -0,0 +1,39 @@
+require 'rubygems'
+require 'activesupport'
+
+spinal_warmup_series_with_meditation = [
+  "get ready to begin: 20 seconds",
+  "hands on ankles, lower back flexes: 2 minutes",
+  "sufi grind right: 1 minute",
+  "switch directions: 1 minute",
+  "hands on shoulders, inhale swing left, exhale swing right: 2 minutes",
+  "hands on knees, upper back flexes: 2 minutes",
+  "neck rotate right: 1 minute",
+  "switch directions: 1 minute",
+  "shoulder shrugs: 2 minutes",
+  "legs straight forward, stretch down on exhale: 2 minutes",
+  "spread legs, stretch toward center: 2 minutes",
+  "spread legs, stretch alternating left and right: 2 minutes",
+  "lay on your left side, right knee to the ground in front of you, look back over right and bring right hand to the floor behind you: 1 minute",
+  "switch sides: 1 minute",
+  "okay, now just relax for 7 minutes: 7 minutes",
+  "get ready for meditation: 20 seconds",
+  "starting eleven minute meditation timer now: 11 minutes",
+  "you're done: 20 seconds",
+]
+
+
+def play(array)
+  
+  array.each do |x|
+    command, timestring = x.split(': ')
+    duration = timestring.split(' ')[0].to_i.send(timestring.split(' ')[1])
+    `say "#{command}"`
+    sleep duration.to_i
+    `say "stop"`
+    sleep 5
+  end
+  
+end
+
+play(spinal_warmup_series_with_meditation)
